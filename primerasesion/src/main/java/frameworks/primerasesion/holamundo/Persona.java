937c1c3e00f031135cef29e560a0442d21fb5711
Raúl Gerardo Vázquez González <deathknight@Fuel.(none)>
2012-11-24 02:44:14 -0600

    Proyecto de sesion 1

---
+++ primerasesion/src/main/java/frameworks/primerasesion/holamundo/Persona.java
@@ -0,0 +1,19 @@
+package frameworks.primerasesion.holamundo;
+
+public class Persona {
+	public String nombre;
+
+	public String execute(){
+		
+		
+		return "SUCCESS";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
